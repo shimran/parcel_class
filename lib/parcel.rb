b04d82ca8668873f0ae26ab9ef0823a0e4942767
Guest User <student@epicodus.com>
2015-01-12 15:22:17 -0800

    Create shallow and worthless shipping algorithm. Passes spec.

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,17 +1,27 @@
 class Parcel
 
-  define_method (:initialize) do |length,width,height|
-  @length= length
-  @width= width
-  @height= height
-
+  define_method (:initialize) do |length,width,height,weight|
+    @length= length
+    @width= width
+    @height= height
+    @weight=weight
   end
 
   define_method (:volume) do
-      volume= @length*@width*@height
+    volume= @length*@width*@height
   end
 
+  define_method(:cost_to_ship) do
+    min_charge=8 #Min charge for first ten pounds
+    ship_cost= @weight
 
+    if ship_cost <= 5
+      min_charge
+    else
+      ship_cost
+    end
+
+  end
 
 
 end #End class
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -6,10 +6,14 @@ require('pry')
   describe("#volume") do
 
     it("returns the products of all the dimensions of the box") do
-      test_parcel = Parcel.new(5, 6, 5)
+      test_parcel = Parcel.new(5, 6, 5, 11)
       expect(test_parcel.volume()).to(eq(150))
     end
 
+    it("returns a calculated shipping cost based on the weight parameter") do
+      test_parcel = Parcel.new(5, 6, 5, 11)
+      expect(test_parcel.cost_to_ship()).to(eq(11))
+    end
   end
 
  end
